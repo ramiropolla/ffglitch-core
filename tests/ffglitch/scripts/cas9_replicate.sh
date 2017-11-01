39cb0b4dad8ad5f609a78d9bfc89caba4725b9a5
Ramiro Polla <ramiro.polla@gmail.com>
2017-11-02 00:15:59 +0100

    glitchr-cas9: use -flags +bitexact for input files in tests

--- tests/ffglitch/scripts/cas9_replicate.sh
+++ tests/ffglitch/scripts/cas9_replicate.sh
@@ -6,5 +6,5 @@ set -e
 OUTPUT=$(mktemp)
 
 ./ffglitch_g -t -hide_banner "$2" ${OUTPUT}
-./ffmpeg_g -hide_banner -i ${OUTPUT} -flags +bitexact -fflags +bitexact -idct simple -f framecrc - > "$1"
+./ffmpeg_g -hide_banner -flags +bitexact -i ${OUTPUT} -flags +bitexact -fflags +bitexact -idct simple -f framecrc - > "$1"
 cmp -s "$2" ${OUTPUT} || { echo "REPLICATION MISMATCH" >> "$1" && false; }
