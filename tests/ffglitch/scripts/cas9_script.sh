219e7dfc956851f2df123d968e773539a852cfee
Ramiro Polla <ramiro.polla@gmail.com>
2017-11-18 14:32:20 -0200

    tests: threads 1

--- tests/ffglitch/scripts/cas9_script.sh
+++ tests/ffglitch/scripts/cas9_script.sh
@@ -9,4 +9,4 @@ OUTPUT=$(mktemp)
 ./ffglitch_g -t -hide_banner "$2" $4 -e "$3"
 ./src/tests/ffglitch/scripts/"$5" "$3" > ${TMPJSON}
 ./ffglitch_g -t -hide_banner "$2" $4 -a ${TMPJSON} ${OUTPUT}
-./ffmpeg_g -hide_banner -flags +bitexact -i ${OUTPUT} -flags +bitexact -fflags +bitexact -idct simple -f framecrc - > "$1"
+./ffmpeg_g -hide_banner -flags +bitexact -threads 1 -i ${OUTPUT} -flags +bitexact -fflags +bitexact -idct simple -f framecrc - > "$1"
--- tests/ffglitch/scripts/cas9_transplicate.sh
+++ tests/ffglitch/scripts/cas9_transplicate.sh
@@ -8,5 +8,5 @@ OUTPUT=$(mktemp)
 
 ./ffglitch_g -t -hide_banner "$2" -e ${TMPJSON}
 ./ffglitch_g -t -hide_banner "$2" -a ${TMPJSON} ${OUTPUT}
-./ffmpeg_g -hide_banner -flags +bitexact -i ${OUTPUT} -flags +bitexact -fflags +bitexact -idct simple -f framecrc - > "$1"
+./ffmpeg_g -hide_banner -flags +bitexact -threads 1 -i ${OUTPUT} -flags +bitexact -fflags +bitexact -idct simple -f framecrc - > "$1"
 cmp -s "$2" ${OUTPUT} || { echo "TRANSPLICATION MISMATCH" >> "$1" && false; }
