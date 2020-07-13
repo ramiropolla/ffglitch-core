2ab7f1c031c25f82ec01b516c308b0222bb7a96a
Ramiro Polla <ramiro.polla@gmail.com>
2020-07-13 12:10:58 +0200

    ffedit: quickjs: disable module loader

--- libavutil/quickjs/quickjs-libc.c
+++ libavutil/quickjs/quickjs-libc.c
@@ -453,7 +453,7 @@ typedef JSModuleDef *(JSInitModuleFunc)(JSContext *ctx,
                                         const char *module_name);
 
 
-#if defined(_WIN32)
+#if 1
 static JSModuleDef *js_module_loader_so(JSContext *ctx,
                                         const char *module_name)
 {
