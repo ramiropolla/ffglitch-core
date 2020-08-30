ramiropolla/ffglitch-core

commit c6f1baf5e38291fd5a6c3b412301d9a97d6b4a89

ffedit: tie output data to input packets
I don't know how I let this pass before.

fftools/ffedit.c
@@ -407,12 +407,6 @@ static int ffedit_decode(
         return ret;
     }
 
-    if ( ectx != NULL && dctx->ffedit_out != NULL )
-    {
-        ffe_output_packet(ectx, ipkt->pos, ipkt->size, dctx->ffedit_out, dctx->ffedit_out_size);
-        av_freep(&dctx->ffedit_out);
-    }
-
     while ( ret >= 0 )
     {
         ret = avcodec_receive_frame(dctx, iframe);
@@ -422,6 +416,12 @@ static int ffedit_decode(
         {
             av_log(NULL, AV_LOG_FATAL, "receive_frame() failed\n");
             return ret;
+        }
+
+        if ( ectx != NULL && dctx->ffedit_out != NULL )
+        {
+            ffe_output_packet(ectx, dctx->ffedit_in_pos, dctx->ffedit_in_size, dctx->ffedit_out, dctx->ffedit_out_size);
+            av_freep(&dctx->ffedit_out);
         }
 
         if ( is_exporting )

libavcodec/avcodec.h
@@ -1655,6 +1655,8 @@ typedef struct AVCodecContext {
      */
     unsigned int codec_tag;
 
+    int64_t ffedit_in_pos;
+    size_t ffedit_in_size;
     void *ffedit_out;
     size_t ffedit_out_size;
 

libavcodec/ffedit_xp.c
@@ -86,6 +86,8 @@ void ffe_transplicate_flush(
         return;
     bitcount = put_bits_count(xp->pb);
     flush_put_bits(xp->pb);
+    avctx->ffedit_in_pos = pkt->pos;
+    avctx->ffedit_in_size = pkt->size;
     avctx->ffedit_out_size = (bitcount + 7) >> 3;
     avctx->ffedit_out = av_malloc(avctx->ffedit_out_size);
     memcpy(avctx->ffedit_out, xp->data, avctx->ffedit_out_size);

libavcodec/pthread_frame.c
@@ -296,6 +296,11 @@ static int update_context_from_thread(AVCodecContext *dst, AVCodecContext *src, 
         }
 
         dst->hwaccel_flags = src->hwaccel_flags;
+
+        dst->ffedit_in_pos = src->ffedit_in_pos;
+        dst->ffedit_in_size = src->ffedit_in_size;
+        dst->ffedit_out = src->ffedit_out;
+        dst->ffedit_out_size = src->ffedit_out_size;
     }
 
     if (for_user) {

tests/ffedit/ref/mpeg2video_prepared_128_5_mb.ref
@@ -3,8 +3,8 @@
 #codec_id 0: rawvideo
 #dimensions 0: 128x128
 #sar 0: 4/3
-0,          1,          1,        1,    24576, 0xa590edbd
-0,          2,          2,        1,    24576, 0xcacfe561
-0,          3,          3,        1,    24576, 0x783ef10c
-0,          4,          4,        1,    24576, 0xbd5825c3
-0,          5,          5,        1,    24576, 0xed9448c5
+0,          1,          1,        1,    24576, 0x56a07b58
+0,          2,          2,        1,    24576, 0x3a38f9da
+0,          3,          3,        1,    24576, 0x9b1502ba
+0,          4,          4,        1,    24576, 0xf3bd3ccc
+0,          5,          5,        1,    24576, 0xff0c6184

tests/ffedit/ref/mpeg2video_prepared_128_5_q_dct.ref
@@ -3,8 +3,8 @@
 #codec_id 0: rawvideo
 #dimensions 0: 128x128
 #sar 0: 4/3
-0,          1,          1,        1,    24576, 0x562877ac
-0,          2,          2,        1,    24576, 0x51aec0c1
-0,          3,          3,        1,    24576, 0x7fc6e130
-0,          4,          4,        1,    24576, 0xcf951c3a
-0,          5,          5,        1,    24576, 0x3aef5b4a
+0,          1,          1,        1,    24576, 0x56a07b58
+0,          2,          2,        1,    24576, 0x376eae49
+0,          3,          3,        1,    24576, 0xe77cbc15
+0,          4,          4,        1,    24576, 0xc85fe8c9
+0,          5,          5,        1,    24576, 0xe3db1c8c

tests/ffedit/ref/mpeg2video_prepared_128_5_qscale.ref
@@ -3,8 +3,8 @@
 #codec_id 0: rawvideo
 #dimensions 0: 128x128
 #sar 0: 4/3
-0,          1,          1,        1,    24576, 0x09aa74a6
-0,          2,          2,        1,    24576, 0x77a9c199
-0,          3,          3,        1,    24576, 0xaa69b4c5
-0,          4,          4,        1,    24576, 0x9728f0c6
-0,          5,          5,        1,    24576, 0x56354fce
+0,          1,          1,        1,    24576, 0x56a07b58
+0,          2,          2,        1,    24576, 0x670dcf2c
+0,          3,          3,        1,    24576, 0x0009c1bc
+0,          4,          4,        1,    24576, 0x828efe0f
+0,          5,          5,        1,    24576, 0x4ed85d73

tests/ffedit/ref/mpeg2video_traffic_128_5_mb.ref
@@ -3,8 +3,8 @@
 #codec_id 0: rawvideo
 #dimensions 0: 128x128
 #sar 0: 4/3
-0,          1,          1,        1,    24576, 0xa590edbd
-0,          2,          2,        1,    24576, 0x9ee7d9e3
+0,          1,          1,        1,    24576, 0x56a07b58
+0,          2,          2,        1,    24576, 0xaef70b6d
 0,          3,          3,        1,    24576, 0x5138e8e7
 0,          4,          4,        1,    24576, 0x6bd678d1
 0,          5,          5,        1,    24576, 0xd6281972

commit b3f7fa42b69f3c55f9a370ded83c06a191ea1484

ffedit: fix skipping of output frames
This was broken in c6f1baf5e38291fd5a6c3b412301d9a97d6b4a89 (ffedit: tie output data to input packets)

There could be only one output packet in AVCodecContext. This was a bad
idea since the decoder could decode multiple frames before reporting
having output, and the output data from previous frames would be
overwritten in AVCodecContext. There is now a list of packets.

fftools/ffedit.c
@@ -826,10 +826,20 @@ static int ffedit_decode(
             return ret;
         }
 
-        if ( fff->ectx != NULL && dctx->ffedit_out != NULL )
+        if ( fff->ectx != NULL && dctx->ffe_xp_packets )
         {
-            ffe_output_packet(fff->ectx, dctx->ffedit_in_pos, dctx->ffedit_in_size, dctx->ffedit_out, dctx->ffedit_out_size);
-            av_freep(&dctx->ffedit_out);
+            for ( size_t i = 0; i < dctx->nb_ffe_xp_packets; i++ )
+            {
+                FFEditTransplicatePacket *opkt = &dctx->ffe_xp_packets[i];
+                ffe_output_packet(fff->ectx,
+                                  opkt->i_pos,
+                                  opkt->i_size,
+                                  opkt->data,
+                                  opkt->o_size);
+                av_freep(&opkt->data);
+            }
+            av_freep(&dctx->ffe_xp_packets);
+            dctx->nb_ffe_xp_packets = 0;
         }
 
         if ( fff->is_exporting )

libavcodec/avcodec.h
@@ -1594,6 +1594,13 @@ typedef struct AVPacket {
 #define AV_PKT_FLAG_DISPOSABLE 0x0010
 
 
+typedef struct FFEditTransplicatePacket {
+    int64_t i_pos;
+    size_t i_size;
+    uint8_t *data;
+    size_t o_size;
+} FFEditTransplicatePacket;
+
 enum AVSideDataParamChangeFlags {
     AV_SIDE_DATA_PARAM_CHANGE_CHANNEL_COUNT  = 0x0001,
     AV_SIDE_DATA_PARAM_CHANGE_CHANNEL_LAYOUT = 0x0002,
@@ -1655,10 +1662,8 @@ typedef struct AVCodecContext {
      */
     unsigned int codec_tag;
 
-    int64_t ffedit_in_pos;
-    size_t ffedit_in_size;
-    void *ffedit_out;
-    size_t ffedit_out_size;
+    FFEditTransplicatePacket *ffe_xp_packets;
+    size_t                 nb_ffe_xp_packets;
 
     void *priv_data;
 

libavcodec/ffedit_xp.c
@@ -81,23 +81,42 @@ void ffe_transplicate_flush(
         FFEditTransplicateContext *xp,
         AVPacket *pkt)
 {
+    FFEditTransplicatePacket *packet;
+    size_t nb_ffe_xp_packets;
+    size_t idx;
     int bitcount;
+    int ret;
+
     if ( xp->pb == NULL )
         return;
+
     bitcount = put_bits_count(xp->pb);
     flush_put_bits(xp->pb);
-    avctx->ffedit_in_pos = pkt->pos;
-    avctx->ffedit_in_size = pkt->size;
-    avctx->ffedit_out_size = (bitcount + 7) >> 3;
-    avctx->ffedit_out = av_malloc(avctx->ffedit_out_size);
-    memcpy(avctx->ffedit_out, xp->data, avctx->ffedit_out_size);
+
+    nb_ffe_xp_packets = avctx->nb_ffe_xp_packets;
+    idx = nb_ffe_xp_packets++;
+
+    ret = av_reallocp_array(&avctx->ffe_xp_packets, nb_ffe_xp_packets, sizeof(FFEditTransplicatePacket));
+    if ( ret < 0 )
+        return;
+
+    avctx->nb_ffe_xp_packets = nb_ffe_xp_packets;
+
+    packet = &avctx->ffe_xp_packets[idx];
+    packet->i_pos = pkt->pos;
+    packet->i_size = pkt->size;
+    packet->o_size = (bitcount + 7) >> 3;
+    packet->data = av_malloc(packet->o_size);
+    memcpy(packet->data, xp->data, packet->o_size);
+
     ffe_transplicate_free(xp);
+
 #if CONFIG_FFEDIT_XP_DEBUG
     /* Check only when replicating */
     if ( avctx->ffedit_apply == (1 << FFEDIT_FEAT_LAST) )
     {
-        const uint8_t *inptr = avctx->ffedit_out;
-        size_t out_size = avctx->ffedit_out_size;
+        const uint8_t *inptr = packet->data;
+        size_t out_size = packet->o_size;
         for ( int i = 0; i < out_size; i++ )
         {
             if ( inptr[i] != pkt->data[i] )

libavcodec/pthread_frame.c
@@ -296,11 +296,6 @@ static int update_context_from_thread(AVCodecContext *dst, AVCodecContext *src, 
         }
 
         dst->hwaccel_flags = src->hwaccel_flags;
-
-        dst->ffedit_in_pos = src->ffedit_in_pos;
-        dst->ffedit_in_size = src->ffedit_in_size;
-        dst->ffedit_out = src->ffedit_out;
-        dst->ffedit_out_size = src->ffedit_out_size;
     }
 
     if (for_user) {

tests/ffedit/ref/mpeg2video_prepared_128_5_mb.ref
@@ -3,8 +3,8 @@
 #codec_id 0: rawvideo
 #dimensions 0: 128x128
 #sar 0: 4/3
-0,          1,          1,        1,    24576, 0x56a07b58
-0,          2,          2,        1,    24576, 0x3a38f9da
-0,          3,          3,        1,    24576, 0x9b1502ba
-0,          4,          4,        1,    24576, 0xf3bd3ccc
-0,          5,          5,        1,    24576, 0xff0c6184
+0,          1,          1,        1,    24576, 0xa590edbd
+0,          2,          2,        1,    24576, 0xcacfe561
+0,          3,          3,        1,    24576, 0x783ef10c
+0,          4,          4,        1,    24576, 0xbd5825c3
+0,          5,          5,        1,    24576, 0xed9448c5

tests/ffedit/ref/mpeg2video_prepared_128_5_q_dct.ref
@@ -3,8 +3,8 @@
 #codec_id 0: rawvideo
 #dimensions 0: 128x128
 #sar 0: 4/3
-0,          1,          1,        1,    24576, 0x56a07b58
-0,          2,          2,        1,    24576, 0x376eae49
-0,          3,          3,        1,    24576, 0xe77cbc15
-0,          4,          4,        1,    24576, 0xc85fe8c9
-0,          5,          5,        1,    24576, 0xe3db1c8c
+0,          1,          1,        1,    24576, 0x562877ac
+0,          2,          2,        1,    24576, 0x51aec0c1
+0,          3,          3,        1,    24576, 0x7fc6e130
+0,          4,          4,        1,    24576, 0xcf951c3a
+0,          5,          5,        1,    24576, 0x3aef5b4a

tests/ffedit/ref/mpeg2video_prepared_128_5_qscale.ref
@@ -3,8 +3,8 @@
 #codec_id 0: rawvideo
 #dimensions 0: 128x128
 #sar 0: 4/3
-0,          1,          1,        1,    24576, 0x56a07b58
-0,          2,          2,        1,    24576, 0x670dcf2c
-0,          3,          3,        1,    24576, 0x0009c1bc
-0,          4,          4,        1,    24576, 0x828efe0f
-0,          5,          5,        1,    24576, 0x4ed85d73
+0,          1,          1,        1,    24576, 0x09aa74a6
+0,          2,          2,        1,    24576, 0x77a9c199
+0,          3,          3,        1,    24576, 0xaa69b4c5
+0,          4,          4,        1,    24576, 0x9728f0c6
+0,          5,          5,        1,    24576, 0x56354fce

tests/ffedit/ref/mpeg2video_traffic_128_5_mb.ref
@@ -3,8 +3,8 @@
 #codec_id 0: rawvideo
 #dimensions 0: 128x128
 #sar 0: 4/3
-0,          1,          1,        1,    24576, 0x56a07b58
-0,          2,          2,        1,    24576, 0xaef70b6d
+0,          1,          1,        1,    24576, 0xa590edbd
+0,          2,          2,        1,    24576, 0x9ee7d9e3
 0,          3,          3,        1,    24576, 0x5138e8e7
 0,          4,          4,        1,    24576, 0x6bd678d1
 0,          5,          5,        1,    24576, 0xd6281972

tests/ffedit/ref/mpeg2video_traffic_128_5_q_dct.ref
@@ -3,8 +3,8 @@
 #codec_id 0: rawvideo
 #dimensions 0: 128x128
 #sar 0: 4/3
-0,          1,          1,        1,    24576, 0x56a07b58
-0,          2,          2,        1,    24576, 0x376eae49
+0,          1,          1,        1,    24576, 0x562877ac
+0,          2,          2,        1,    24576, 0x51aec0c1
 0,          3,          3,        1,    24576, 0x044eb34d
 0,          4,          4,        1,    24576, 0xd604f308
 0,          5,          5,        1,    24576, 0x1612f718

tests/ffedit/ref/mpeg2video_traffic_128_5_qscale.ref
@@ -3,8 +3,8 @@
 #codec_id 0: rawvideo
 #dimensions 0: 128x128
 #sar 0: 4/3
-0,          1,          1,        1,    24576, 0x56a07b58
-0,          2,          2,        1,    24576, 0x670dcf2c
+0,          1,          1,        1,    24576, 0x09aa74a6
+0,          2,          2,        1,    24576, 0x77a9c199
 0,          3,          3,        1,    24576, 0xc72592d8
 0,          4,          4,        1,    24576, 0x9b6cc82a
 0,          5,          5,        1,    24576, 0xdca0d0e0