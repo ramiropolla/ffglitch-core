743fe0f4ae06f786a0f6be04b8ffbf288e035913
Ramiro Polla <ramiro.polla@gmail.com>
2021-01-09 11:18:57 +0100

    Update ffglitch copyright string

--- fftools/cmdutils.c
+++ fftools/cmdutils.c
@@ -1156,7 +1156,7 @@ static void print_program_info(int flags, int level)
                program_birth_year, CONFIG_THIS_YEAR);
     av_log(NULL, level, "\n");
     if (flags & SHOW_COPYRIGHT)
-        av_log(NULL, level, "  FFedit <http://ffglitch.org> Copyright (c) 2017-2020 Ramiro Polla\n");
+        av_log(NULL, level, "  FFglitch Copyright (c) 2017-2021 Ramiro Polla\n");
     av_log(NULL, level, "%sbuilt with %s\n", indent, CC_IDENT);
 
     av_log(NULL, level, "%sconfiguration: " FFMPEG_CONFIGURATION "\n", indent);
