36b8144c65170638807f94ea5a2ea38b49e5b676
Ramiro Polla <ramiro.polla@gmail.com>
2021-07-02 17:09:59 +0200

    ffedit: speedup json_fputs() by a lot (around 80%)
    Use a big buffer to avoid fprintf()ing all the time.
    Also add some optimized small-number output.

--- libavutil/json_dump.c
+++ libavutil/json_dump.c
@@ -1,91 +1,191 @@
 /* Crappy JSON dumper
- * Copyright (c) 2018-2019 Ramiro Polla
+ * Copyright (c) 2018-2021 Ramiro Polla
  * MIT License
  */
+
+#include <stdlib.h>
+#include <stdarg.h>
 
 #include "json.h"
 
 //---------------------------------------------------------------------
-static void output_lf(FILE *fp, json_t *jso, int level)
+#define CHUNK_SIZE 4 * 1024 * 1024
+typedef struct sbuf
 {
-    if ( (jso->flags & JSON_PFLAGS_NO_LF) == 0 )
+    FILE *fp;
+    char *data;
+    size_t offset;
+} sbuf;
+
+static inline void sbuf_init(sbuf *ctx, FILE *fp)
+{
+    ctx->fp = fp;
+    ctx->data = malloc(CHUNK_SIZE);
+    ctx->offset = 0;
+}
+
+static inline void sbuf_flush(sbuf *ctx)
+{
+    if ( ctx->offset != 0 )
     {
-        fprintf(fp, "\n");
-        fprintf(fp, "%*s", level * 2, "");
-    }
-    else if ( (jso->flags & JSON_PFLAGS_NO_SPACE) == 0 )
-    {
-        fprintf(fp, " ");
+        fwrite(ctx->data, ctx->offset, 1, ctx->fp);
+        ctx->offset = 0;
     }
 }
 
-static void json_print_element(FILE *fp, json_t *jso, int level)
+static inline void sbuf_fputc(sbuf *ctx, char c)
+{
+    if ( ctx->offset == CHUNK_SIZE )
+        sbuf_flush(ctx);
+    ctx->data[ctx->offset++] = c;
+}
+
+static inline void sbuf_fputs(sbuf *ctx, const char *str)
+{
+    while ( *str != '\0' )
+        sbuf_fputc(ctx, *str++);
+}
+
+static inline void sbuf_fprintf(sbuf *ctx, const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    sbuf_flush(ctx);
+    vfprintf(ctx->fp, format, args);
+    va_end(args);
+}
+
+static inline void sbuf_spaces(sbuf *ctx, size_t num)
+{
+    if ( ctx->offset + num >= CHUNK_SIZE )
+        sbuf_flush(ctx);
+    while ( num-- )
+        ctx->data[ctx->offset++] = ' ';
+}
+
+static inline void sbuf_free(sbuf *ctx)
+{
+    sbuf_flush(ctx);
+    free(ctx->data);
+}
+
+//---------------------------------------------------------------------
+static void output_lf(sbuf *ctx, json_t *jso, int level)
+{
+    if ( (jso->flags & JSON_PFLAGS_NO_LF) == 0 )
+    {
+        sbuf_fputc(ctx, '\n');
+        sbuf_spaces(ctx, level * 2);
+    }
+    else if ( (jso->flags & JSON_PFLAGS_NO_SPACE) == 0 )
+    {
+        sbuf_fputc(ctx, ' ');
+    }
+}
+
+static void output_num(sbuf *ctx, int64_t val)
+{
+    if ( val == JSON_NULL )
+    {
+        sbuf_fputs(ctx, "null");
+    }
+    else
+    {
+        int is_neg = (val < 0);
+        uint64_t abs_val = (uint64_t) (is_neg ? -val : val);
+        if ( abs_val < 10 )
+        {
+            uint8_t u8 = (uint8_t) abs_val;
+            if ( is_neg )
+                sbuf_fputc(ctx, '-');
+            sbuf_fputc(ctx, '0' + u8);
+        }
+        else if ( abs_val < 100 )
+        {
+            uint8_t u8 = (uint8_t) abs_val;
+            if ( is_neg )
+                sbuf_fputc(ctx, '-');
+            sbuf_fputc(ctx, '0' + u8 / 10);
+            sbuf_fputc(ctx, '0' + u8 % 10);
+        }
+        else
+        {
+            sbuf_fprintf(ctx, "%" PRId64, val);
+        }
+    }
+}
+
+static void output_string(sbuf *ctx, const char *str)
+{
+    sbuf_fputc(ctx, '"');
+    sbuf_fputs(ctx, str);
+    sbuf_fputc(ctx, '"');
+}
+
+static void json_print_element(sbuf *ctx, json_t *jso, int level)
 {
     if ( jso == NULL )
     {
-        fprintf(fp, "null");
+        sbuf_fputs(ctx, "null");
         return;
     }
     switch ( JSON_TYPE(jso->flags) )
     {
     case JSON_TYPE_OBJECT:
-        fprintf(fp, "{");
+        sbuf_fputc(ctx, '{');
         for ( size_t i = 0; i < JSON_LEN(jso->flags); i++ )
         {
             if ( i != 0 )
-                fprintf(fp, ",");
-            output_lf(fp, jso, level+1);
-            fprintf(fp, "\"%s\"", jso->obj->names[i]);
-            fprintf(fp, ":");
-            json_print_element(fp, jso->obj->values[i], level+1);
+                sbuf_fputc(ctx, ',');
+            output_lf(ctx, jso, level+1);
+            output_string(ctx, jso->obj->names[i]);
+            sbuf_fputc(ctx, ':');
+            json_print_element(ctx, jso->obj->values[i], level+1);
         }
-        output_lf(fp, jso, level);
-        fprintf(fp, "}");
+        output_lf(ctx, jso, level);
+        sbuf_fputc(ctx, '}');
         break;
     case JSON_TYPE_ARRAY:
-        fprintf(fp, "[");
+        sbuf_fputc(ctx, '[');
         for ( size_t i = 0; i < JSON_LEN(jso->flags); i++ )
         {
             if ( i != 0 )
-                fprintf(fp, ",");
-            output_lf(fp, jso, level+1);
-            json_print_element(fp, jso->array[i], level+1);
+                sbuf_fputc(ctx, ',');
+            output_lf(ctx, jso, level+1);
+            json_print_element(ctx, jso->array[i], level+1);
         }
-        output_lf(fp, jso, level);
-        fprintf(fp, "]");
+        output_lf(ctx, jso, level);
+        sbuf_fputc(ctx, ']');
         break;
     case JSON_TYPE_ARRAY_OF_INTS:
-        fprintf(fp, "[");
+        sbuf_fputc(ctx, '[');
         for ( size_t i = 0; i < JSON_LEN(jso->flags); i++ )
         {
             if ( i != 0 )
-                fprintf(fp, ",");
-            output_lf(fp, jso, level+1);
-            if ( jso->array_of_ints[i] == JSON_NULL )
-                fprintf(fp, "null");
-            else
-                fprintf(fp, "%" PRId64, jso->array_of_ints[i]);
+                sbuf_fputc(ctx, ',');
+            output_lf(ctx, jso, level+1);
+            output_num(ctx, jso->array_of_ints[i]);
         }
-        output_lf(fp, jso, level);
-        fprintf(fp, "]");
+        output_lf(ctx, jso, level);
+        sbuf_fputc(ctx, ']');
         break;
     case JSON_TYPE_STRING:
-        fprintf(fp, "\"%s\"", jso->str);
+        output_string(ctx, jso->str);
         break;
     case JSON_TYPE_NUMBER:
-        if ( jso->val == JSON_NULL )
-            fprintf(fp, "null");
-        else
-            fprintf(fp, "%" PRId64, jso->val);
+        output_num(ctx, jso->val);
         break;
     case JSON_TYPE_BOOL:
-        fprintf(fp, (jso->val == 0) ? "false" : "true");
+        sbuf_fputs(ctx, (jso->val == 0) ? "false" : "true");
         break;
     }
 }
 
 void json_fputs(FILE *fp, json_t *jso)
 {
-    json_print_element(fp, jso, 0);
-    fprintf(fp, "\n");
+    sbuf ctx;
+    sbuf_init(&ctx, fp);
+    json_print_element(&ctx, jso, 0);
+    sbuf_fputc(&ctx, '\n');
+    sbuf_free(&ctx);
 }
