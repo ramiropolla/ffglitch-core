893ff523f56380db534df33036c7ce486b5005af
Ramiro Polla <ramiro.polla@gmail.com>
2023-11-05 21:47:53 +0100

    fflive: set duration of first frame to zero. we want to immediately show the second frame to reduce latency

--- fftools/ffplay.c
+++ fftools/ffplay.c
@@ -1571,7 +1571,7 @@ static double compute_target_delay(double delay, VideoState *is)
 static double vp_duration(VideoState *is, Frame *vp, Frame *nextvp) {
     if (vp->serial == nextvp->serial) {
         double duration = nextvp->pts - vp->pts;
-        if (isnan(duration) || duration <= 0 || duration > is->max_frame_duration)
+        if (isnan(duration) || duration < 0 || duration > is->max_frame_duration)
             return vp->duration;
         else
             return duration;
