4ec68453a0c2d34341110e5f66a62e103f39b132
Ramiro Polla <ramiro.polla@gmail.com>
2023-11-05 21:46:18 +0100

    fflive: reduce latency while waiting for frame_queue

--- fftools/ffplay.c
+++ fftools/ffplay.c
@@ -3311,8 +3311,15 @@ static void refresh_loop_wait_event(VideoState *is, SDL_Event *event) {
             SDL_ShowCursor(0);
             cursor_hidden = 1;
         }
-        if (remaining_time > 0.0)
-            av_usleep((int64_t)(remaining_time * 1000000.0));
+        if (remaining_time > 0.0) {
+            if (is->video_st) {
+                SDL_LockMutex(is->pictq.mutex);
+                SDL_CondWaitTimeout(is->pictq.cond, is->pictq.mutex, (uint32_t)(remaining_time * 1000.0));
+                SDL_UnlockMutex(is->pictq.mutex);
+            } else {
+                av_usleep((int64_t)(remaining_time * 1000000.0));
+            }
+        }
         remaining_time = REFRESH_RATE;
         if (is->show_mode != SHOW_MODE_NONE && (!is->paused || is->force_refresh))
             video_refresh(is, &remaining_time);
