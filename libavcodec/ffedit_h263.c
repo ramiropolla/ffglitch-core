cdf2553f241db85f3568d4d8b4f153e4322ff6fa
Ramiro Polla <ramiro.polla@gmail.com>
2024-06-24 16:48:49 +0200

    ffedit: libavcodec/mjpeg: use json_const_object_from
    fixup to "ffedit: libavcodec/mpeg4: add exporting of info"
    commit 524899fda044a7d5e675f08ebebe83a3f5627ec0

--- libavcodec/ffedit_h263.c
+++ libavcodec/ffedit_h263.c
@@ -16,33 +16,21 @@
 static void
 ffe_h263_export_info_init(MpegEncContext *s, AVFrame *f)
 {
-    json_t *jframe = json_object_new(s->jctx);
-    json_t *jso;
     const char *pict_type = s->pict_type == AV_PICTURE_TYPE_I ? "I"
                           : s->pict_type == AV_PICTURE_TYPE_P ? "P"
                           : s->pict_type == AV_PICTURE_TYPE_B ? "B"
                           :                                     "S";
-
-    jso = json_string_new(s->jctx, pict_type);
-
-    json_object_add(jframe, "pict_type", jso);
-
-    jso = ffe_jblock_new(s->jctx,
-                         s->mb_width, s->mb_height,
-                         JSON_PFLAGS_NO_LF);
-
-    json_object_add(jframe, "mb_type", jso);
-
+    json_t *jpict_type = json_string_new(s->jctx, pict_type);
+    json_t *jmb_type = ffe_jblock_new(s->jctx,
+                                      s->mb_width, s->mb_height,
+                                      JSON_PFLAGS_NO_LF);
+    json_kvp_t kvps[] = {
+        { "pict_type", jpict_type },
+        { "mb_type", jmb_type },
+        { NULL }
+    };
+    json_t *jframe = json_const_object_from(s->jctx, kvps);
     f->ffedit_sd[FFEDIT_FEAT_INFO] = jframe;
-}
-
-/* cleanup */
-
-static void
-ffe_h263_export_info_cleanup(MpegEncContext *s, AVFrame *f)
-{
-    json_t *jframe = f->ffedit_sd[FFEDIT_FEAT_INFO];
-    json_object_done(s->jctx, jframe);
 }
 
 /*-------------------------------------------------------------------*/
@@ -125,9 +113,6 @@ ffe_h263_export_init(MpegEncContext *s)
 static void
 ffe_h263_export_cleanup(MpegEncContext *s, AVFrame *f)
 {
-    if ( (s->avctx->ffedit_export & (1 << FFEDIT_FEAT_INFO)) != 0 )
-        ffe_h263_export_info_cleanup(s, f);
-
     if ( (s->avctx->ffedit_export & (1 << FFEDIT_FEAT_MV)) != 0 )
         ffe_mv_export_cleanup(s->jctx, f);
 
