FFmpeg/FFmpeg

avutil/aarch64: add AV_COPY128 and AV_ZERO128 macros

libavutil/intreadwrite.h
@@ -64,7 +64,9 @@ typedef union {
 
 #include "config.h"
 
-#if ARCH_MIPS
+#if ARCH_AARCH64
+#   include "aarch64/intreadwrite.h"
+#elif ARCH_MIPS
 #   include "mips/intreadwrite.h"
 #elif ARCH_PPC
 #   include "ppc/intreadwrite.h"

ramiropolla/ffglitch-core

libavutil/intreadwrite.h
@@ -64,7 +64,9 @@ typedef union {
 
 #include "config.h"
 
-#if   ARCH_ARM
+#if   ARCH_AARCH64
+#   include "aarch64/intreadwrite.h"
+#elif ARCH_ARM
 #   include "arm/intreadwrite.h"
 #elif ARCH_AVR32
 #   include "avr32/intreadwrite.h"